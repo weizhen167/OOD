a3fa2a1ca94726a4df30c94657a6b925b2870416
weizhen167 <weizhen167@gmail.com>
2016-03-23 21:12:58 -0400

    w

--- hw9/src/ConsoleMain.java
+++
@@ -1,6 +0,0 @@
-
-public class ConsoleMain {
-    public static void main(String args[]) throws Exception {
-        new Controller(Draught.make8x8game(), System.in, System.out).playGame();
-    }
-}
--- hw9/src/ServerMain.java
+++
@@ -1,6 +0,0 @@
-
-public class ServerMain {
-    public static void main(String args[]) throws Exception {
-        new MyServerFramework().listen();
-    }
-}
